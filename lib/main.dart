5fc264fc70b5d05253d5d6557abb8d159f9fa615
evangel <evangeldaping@gmail.com>
2022-03-10 09:35:07 +0800

    Installed english_words package and use WordPair for random words.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.purple[900]),
         home: Scaffold(
             appBar: AppBar(title: const Text('WordPair Generator')),
-            body: const Center(child: Text('Hello World'))));
+            body: Center(child: Text(wordPair.asPascalCase))));
   }
 }
