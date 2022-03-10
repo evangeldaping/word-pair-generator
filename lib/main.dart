33732fa5944aab4f647c63600959370318a94cc5
evangel <evangeldaping@gmail.com>
2022-03-10 09:31:20 +0800

    Used ThemeData and Scaffold Widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Text('Hello World',
-            style: TextStyle(fontSize: 30.5, color: Colors.green[400])));
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: Scaffold(
+            appBar: AppBar(title: const Text('WordPair Generator')),
+            body: const Center(child: Text('Hello World'))));
   }
 }
